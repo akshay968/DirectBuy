59c7a90d80cd28b4813d076ae42db9dfd569dbbf
akshay968 <76764402+akshay968@users.noreply.github.com>
2023-06-23 14:55:45 +0530

    Adding UI for Login and Signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,11 +4,11 @@ from .models import Account
 class RegistrationForm(forms.ModelForm):
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter Password',
+        'placeholder': '',
         'class': 'form-control',
     }))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Confirm Password',
+        'placeholder': '',
         'class': 'form-control',
         }))
 
@@ -31,10 +31,10 @@ class RegistrationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
-        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
-        self.fields['email'].widget.attrs['placeholder'] = 'Enter Your Email'
-        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['first_name'].widget.attrs['placeholder'] = ''
+        self.fields['last_name'].widget.attrs['placeholder'] = ''
+        self.fields['email'].widget.attrs['placeholder'] = ''
+        self.fields['phone_number'].widget.attrs['placeholder'] = ''
 
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
